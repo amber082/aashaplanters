862929f222ebeb91188cd175d5b83919e39f8f50
amber082 <ambers.cd.cse17@itbhu.ac.in>
2021-05-13 18:36:07 +0530

    Updating setting.py

--- aashaplant/aashaplant/settings.py
+++ aashaplant/aashaplant/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '-=k0ml)h_l7dvovi5*es(og#j1-$ila-t$6tt9_uk+l1e-npb4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['167.71.238.247','127.0.0.1','localhost']
 
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 #MEDIA_URL = '/media/'
